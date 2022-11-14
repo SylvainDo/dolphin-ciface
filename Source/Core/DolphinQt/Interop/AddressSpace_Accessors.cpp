3d7d75758162d5a3417186d8e3924bd10610a458
Sylvain <sylvain.domange@outlook.fr>
2022-11-14 13:43:12 +0100

    Remove global address space accessor

--- Source/Core/DolphinQt/Interop/AddressSpace_Accessors.cpp
+++ Source/Core/DolphinQt/Interop/AddressSpace_Accessors.cpp
@@ -6,77 +6,86 @@ extern dol_calloc_t interop_calloc;
 
 #include "Core/HW/AddressSpace.h"
 
-static AddressSpace::Accessors* _instance;
-
-static void dol_AddressSpace_Accessors_setCurrent(dol_AddressSpace_Type address_space)
+inline AddressSpace::Accessors* Get(dol_AddressSpace_Type address_space)
 {
-  _instance = AddressSpace::GetAccessors(static_cast<AddressSpace::Type>(address_space));
+  return AddressSpace::GetAccessors(static_cast<AddressSpace::Type>(address_space));
 }
 
-static bool dol_AddressSpace_Accessors_isValidAddress(uint32_t address)
+static bool dol_AddressSpace_Accessors_isValidAddress(dol_AddressSpace_Type address_space,
+                                                      uint32_t address)
 {
-  return _instance->IsValidAddress(address);
+  return Get(address_space)->IsValidAddress(address);
 }
 
-static uint8_t dol_AddressSpace_Accessors_readU8(uint32_t address)
+static uint8_t dol_AddressSpace_Accessors_readU8(dol_AddressSpace_Type address_space,
+                                                 uint32_t address)
 {
-  return _instance->ReadU8(address);
+  return Get(address_space)->ReadU8(address);
 }
 
-static void dol_AddressSpace_Accessors_writeU8(uint32_t address, uint8_t value)
+static void dol_AddressSpace_Accessors_writeU8(dol_AddressSpace_Type address_space,
+                                               uint32_t address, uint8_t value)
 {
-  _instance->WriteU8(address, value);
+  Get(address_space)->WriteU8(address, value);
 }
 
-static uint16_t dol_AddressSpace_Accessors_readU16(uint32_t address)
+static uint16_t dol_AddressSpace_Accessors_readU16(dol_AddressSpace_Type address_space,
+                                                   uint32_t address)
 {
-  return _instance->ReadU16(address);
+  return Get(address_space)->ReadU16(address);
 }
 
-static void dol_AddressSpace_Accessors_writeU16(uint32_t address, uint16_t value)
+static void dol_AddressSpace_Accessors_writeU16(dol_AddressSpace_Type address_space,
+                                                uint32_t address, uint16_t value)
 {
-  _instance->WriteU16(address, value);
+  Get(address_space)->WriteU16(address, value);
 }
 
-static uint32_t dol_AddressSpace_Accessors_readU32(uint32_t address)
+static uint32_t dol_AddressSpace_Accessors_readU32(dol_AddressSpace_Type address_space,
+                                                   uint32_t address)
 {
-  return _instance->ReadU32(address);
+  return Get(address_space)->ReadU32(address);
 }
 
-static void dol_AddressSpace_Accessors_writeU32(uint32_t address, uint32_t value)
+static void dol_AddressSpace_Accessors_writeU32(dol_AddressSpace_Type address_space,
+                                                uint32_t address, uint32_t value)
 {
-  _instance->WriteU32(address, value);
+  Get(address_space)->WriteU32(address, value);
 }
 
-static uint64_t dol_AddressSpace_Accessors_readU64(uint32_t address)
+static uint64_t dol_AddressSpace_Accessors_readU64(dol_AddressSpace_Type address_space,
+                                                   uint32_t address)
 {
-  return _instance->ReadU64(address);
+  return Get(address_space)->ReadU64(address);
 }
 
-static void dol_AddressSpace_Accessors_writeU64(uint32_t address, uint64_t value)
+static void dol_AddressSpace_Accessors_writeU64(dol_AddressSpace_Type address_space,
+                                                uint32_t address, uint64_t value)
 {
-  _instance->WriteU64(address, value);
+  Get(address_space)->WriteU64(address, value);
 }
 
-static float dol_AddressSpace_Accessors_readF32(uint32_t address)
+static float dol_AddressSpace_Accessors_readF32(dol_AddressSpace_Type address_space,
+                                                uint32_t address)
 {
-  return _instance->ReadF32(address);
+  return Get(address_space)->ReadF32(address);
 }
 
-static const uint8_t* dol_AddressSpace_Accessors_begin()
+static const uint8_t* dol_AddressSpace_Accessors_begin(dol_AddressSpace_Type address_space)
 {
-  return _instance->begin();
+  return Get(address_space)->begin();
 }
 
-static const uint8_t* dol_AddressSpace_Accessors_end()
+static const uint8_t* dol_AddressSpace_Accessors_end(dol_AddressSpace_Type address_space)
 {
-  return _instance->end();
+  return Get(address_space)->end();
 }
 
-static bool dol_AddressSpace_Accessors_search(uint32_t haystack_offset, const uint8_t* needle_start,
+static bool dol_AddressSpace_Accessors_search(dol_AddressSpace_Type address_space,
+                                              uint32_t haystack_offset, const uint8_t* needle_start,
                                               size_t needle_size, bool forward, uint32_t* result)
 {
-  auto opt = _instance->Search(haystack_offset, needle_start, needle_size, forward);
+  auto opt = Get(address_space)->Search(haystack_offset, needle_start, needle_size, forward);
   if (opt.has_value())
   {
     *result = opt.value();
@@ -89,7 +98,6 @@ EXPORT dol_AddressSpace_Accessors* dol_AddressSpace_Accessors_newInterface()
 {
   auto iface = static_cast<dol_AddressSpace_Accessors*>(
       interop_calloc(1, sizeof(dol_AddressSpace_Accessors)));
-  iface->setCurrent = dol_AddressSpace_Accessors_setCurrent;
   iface->isValidAddress = dol_AddressSpace_Accessors_isValidAddress;
   iface->readU8 = dol_AddressSpace_Accessors_readU8;
   iface->writeU8 = dol_AddressSpace_Accessors_writeU8;
--- Source/Core/DolphinQt/Interop/Interface/dol/AddressSpace_Accessors.h
+++ Source/Core/DolphinQt/Interop/Interface/dol/AddressSpace_Accessors.h
@@ -10,21 +10,20 @@ extern "C" {
 
 struct dol_AddressSpace_Accessors
 {
-  void (*setCurrent)(dol_AddressSpace_Type address_space);
-  bool (*isValidAddress)(uint32_t address);
-  uint8_t (*readU8)(uint32_t address);
-  void (*writeU8)(uint32_t address, uint8_t value);
-  uint16_t (*readU16)(uint32_t address);
-  void (*writeU16)(uint32_t address, uint16_t value);
-  uint32_t (*readU32)(uint32_t address);
-  void (*writeU32)(uint32_t address, uint32_t value);
-  uint64_t (*readU64)(uint32_t address);
-  void (*writeU64)(uint32_t address, uint64_t value);
-  float (*readF32)(uint32_t address);
-  const uint8_t* (*begin)(void);
-  const uint8_t* (*end)(void);
-  bool (*search)(uint32_t haystack_offset, const uint8_t* needle_start, size_t needle_size,
-                 bool forward, uint32_t* result);
+  bool (*isValidAddress)(dol_AddressSpace_Type address_space, uint32_t address);
+  uint8_t (*readU8)(dol_AddressSpace_Type address_space, uint32_t address);
+  void (*writeU8)(dol_AddressSpace_Type address_space, uint32_t address, uint8_t value);
+  uint16_t (*readU16)(dol_AddressSpace_Type address_space, uint32_t address);
+  void (*writeU16)(dol_AddressSpace_Type address_space, uint32_t address, uint16_t value);
+  uint32_t (*readU32)(dol_AddressSpace_Type address_space, uint32_t address);
+  void (*writeU32)(dol_AddressSpace_Type address_space, uint32_t address, uint32_t value);
+  uint64_t (*readU64)(dol_AddressSpace_Type address_space, uint32_t address);
+  void (*writeU64)(dol_AddressSpace_Type address_space, uint32_t address, uint64_t value);
+  float (*readF32)(dol_AddressSpace_Type address_space, uint32_t address);
+  const uint8_t* (*begin)(dol_AddressSpace_Type address_space);
+  const uint8_t* (*end)(dol_AddressSpace_Type address_space);
+  bool (*search)(dol_AddressSpace_Type address_space, uint32_t haystack_offset,
+                 const uint8_t* needle_start, size_t needle_size, bool forward, uint32_t* result);
 };
 
 typedef dol_AddressSpace_Accessors* (*dol_AddressSpace_Accessors_newInterface_t)(void);
